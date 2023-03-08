9ffd258e63b6f358e4a9719a209aee5d8b24c931
cohenta2 <cohen.a.trevor@gmail.com>
2023-03-07 22:31:56 -0500

    Adding graphql

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,42 +1,16 @@
-import { Stack, StackProps, SecretValue, CfnOutput, aws_amplify }  from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { App, GitHubSourceCodeProvider} from '@aws-cdk/aws-amplify-alpha'
-import { BuildSpec } from 'aws-cdk-lib/aws-codebuild';
+import { Stack, StackProps } from "aws-cdk-lib";
+import * as appsync from "aws-cdk-lib/aws-appsync";
+import { Construct } from "constructs";
+import * as path from "path";
 
 export class BackendStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
-	const siteApp = new  App(this, 'SiteApp', {
-		sourceCodeProvider: new GitHubSourceCodeProvider({
-			owner: "Start-Serverless",
-			repository: "WebPage",
-			oauthToken: SecretValue.secretsManager('github-amplify-token')
-		}),
-		environmentVariables: {
-			region: this.region
-		},
-		buildSpec: BuildSpec.fromObjectToYaml({
-				version: 1,
-				frontend: {
-					phases: {
-						preBuild: {
-							commands: ['npm ci'],
-						},
-						build: {
-							commands: ['npm run build'],
-						},
-					},
-					artifacts: {
-						baseDirectory: '/dist',
-						files: ['**/*'],
-					},
-					cache: {
-						paths: ['node_modules/**/*'],
-					},
-				},
-			}),
-	})   
-
-	// const domain = siteApp.addDomain('')
-  }
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
+        const api = new appsync.GraphqlApi(this, "WebsiteAPI", {
+            name: "Websiteapi",
+            schema: appsync.SchemaFile.fromAsset(
+                path.join(__dirname, "graphql/schema.graphql")
+            ),
+        });
+    }
 }
