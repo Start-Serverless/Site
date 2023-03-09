2ffea93811d588e2d233fd3c000470d7fd7ddc7f
cohenta2 <cohen.a.trevor@gmail.com>
2023-03-08 23:06:44 -0500

    Adding dynamo table, and starting resolver

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { Table, AttributeType } from "aws-cdk-lib/aws-dynamodb";
 import * as appsync from "aws-cdk-lib/aws-appsync";
 import { Construct } from "constructs";
 import * as path from "path";
@@ -6,11 +7,22 @@ import * as path from "path";
 export class BackendStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
+
+        const table = new Table(this, "Contacts", {
+            partitionKey: { name: "pk", type: AttributeType.STRING },
+            sortKey: { name: "sk", type: AttributeType.STRING },
+        });
+
         const api = new appsync.GraphqlApi(this, "WebsiteAPI", {
             name: "Websiteapi",
             schema: appsync.SchemaFile.fromAsset(
                 path.join(__dirname, "graphql/schema.graphql")
             ),
+            logConfig: {
+                fieldLogLevel: appsync.FieldLogLevel.ALL,
+            },
         });
+
+        api.addDynamoDbDataSource("contactTable", table);
     }
 }
