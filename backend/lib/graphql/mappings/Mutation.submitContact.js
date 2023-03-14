94e03f5753ce6f5b1c42d6f4f6f978293ed9d794
dsdTCohen <tcohen@dsdpartners.com>
2023-03-13 22:36:28 -0400

    Working submit contact resolver

--- backend/lib/graphql/mappings/Mutation.submitContact.js
+++ backend/lib/graphql/mappings/Mutation.submitContact.js
@@ -1,12 +1,21 @@
 import { util } from "@aws-appsync/utils";
+
 export function request(ctx) {
-    const { phone, email, name } = ctx.args;
-    return {
-        operation: "PutItem",
-        key: util.dynamodb.toMapValues({
-            pk: "CONTACT",
-            sk: `CONTACT#${email}`,
-        }),
-        attributeValues: util.dynamodb.toMapValues(phone, email, name),
-    };
+	const { phone, email, name } = ctx.args;
+	return {
+		operation: "PutItem",
+		key: util.dynamodb.toMapValues({
+			pk: "CONTACT",
+			sk: `CONTACT#${email}`,
+		}),
+		attributeValues: util.dynamodb.toMapValues(phone, email, name),
+	};
+}
+
+export function reqsponse(ctx) {
+	return {
+		phone: ctx.args.phone,
+		email: ctx.args.email,
+		name: ctx.args.name
+	}
 }
