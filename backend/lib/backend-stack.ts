faa7bb9b10d9faabeb15121d68fc73c8f4b84254
cohenta2 <cohen.a.trevor@gmail.com>
2023-03-16 23:12:58 -0400

    Starting dynamodb stream

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,5 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
-import { Table, AttributeType } from "aws-cdk-lib/aws-dynamodb";
+import { Table, AttributeType, StreamViewType } from "aws-cdk-lib/aws-dynamodb";
+import { CfnPipe } from "aws-cdk-lib/aws-pipes";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as appsync from "aws-cdk-lib/aws-appsync";
 import { Construct } from "constructs";
 import * as path from "path";
@@ -11,6 +13,7 @@ export class BackendStack extends Stack {
         const table = new Table(this, "Contacts", {
             partitionKey: { name: "pk", type: AttributeType.STRING },
             sortKey: { name: "sk", type: AttributeType.STRING },
+            stream: StreamViewType.KEYS_ONLY,
         });
 
         const api = new appsync.GraphqlApi(this, "WebsiteAPI", {
@@ -23,20 +26,25 @@ export class BackendStack extends Stack {
             },
         });
 
-        const tableDataSource = api.addDynamoDbDataSource("contactTable", table);
+        const tableDataSource = api.addDynamoDbDataSource(
+            "contactTable",
+            table
+        );
 
-		tableDataSource.createResolver("MutationSubmitcontact", {
-			typeName: "Mutation",
-			fieldName: 'submitContact',
-			requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
-				new appsync.PrimaryKey( 
-					new appsync.Assign("pk", '"CONTACT"'), 
-					new appsync.Assign("sk", '"EMAIL#$!{input.email}"')
-				),
-				appsync.Values.projecting("input")
-			),
-			responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem()
-		})
+        tableDataSource.createResolver("MutationSubmitcontact", {
+            typeName: "Mutation",
+            fieldName: "submitContact",
+            requestMappingTemplate: appsync.MappingTemplate.dynamoDbPutItem(
+                new appsync.PrimaryKey(
+                    new appsync.Assign("pk", '"CONTACT"'),
+                    new appsync.Assign("sk", '"EMAIL#$!{input.email}"')
+                ),
+                appsync.Values.projecting("input")
+            ),
+            responseMappingTemplate:
+                appsync.MappingTemplate.dynamoDbResultItem(),
+        });
 
+        // const sourcePolicy
     }
 }
