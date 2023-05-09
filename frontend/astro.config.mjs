a1b3573c738e288b4808f1a273b0e3e213c2f0ad
dsdTCohen <tcohen@dsdpartners.com>
2023-05-09 15:32:43 -0400

    Improving lighthouse score.  Adding dimensions for images

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 import astroAws from "@astro-aws/adapter";
 
+// https://astro.build/config
 export default defineConfig({
 	output: "server",
 	adapter: astroAws(),
+	//   integrations: [image()]
+	experimental: {
+		assets: true
+	},
 });
